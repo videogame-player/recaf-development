4f7099783f5c3ebec3081a96b3319253cca79771
videogame-player <shalomyeah@protonmail.com>
2021-03-31 15:01:05 +0100

    Add support for adding alternative arguments

--- src/main/java/me/videogame/recaf/RecafExtension.java
+++ src/main/java/me/videogame/recaf/RecafExtension.java
@@ -1,9 +1,13 @@
 package me.videogame.recaf;
 
+import java.util.ArrayList;
+
 public class RecafExtension {
     public String mainClass;
 
     public String recafVersion;
 
     public boolean addRecaf = true;
+
+    public ArrayList<String> recafArgs = new ArrayList<>();
 }
